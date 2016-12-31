065a46d556944c140276f37c1775b3f98c7bd24e
oka <ogiekako@gmail.com>
2017-01-01 04:31:06 +0900

    Add regularized linear regression cost function.

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -18,7 +18,7 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-
+J = sum((X * theta - y) .^ 2) / (2 * m) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
 
 
 
