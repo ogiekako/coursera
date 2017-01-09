d34440539cc2d2eebc25f1010f83a476b4990edd
oka <ogiekako@gmail.com>
2017-01-09 16:08:02 +0900

    Confirmed my own sample emails are correctly classified.

--- ex6/ex6_spam.m
+++ ex6/ex6_spam.m
@@ -125,14 +125,16 @@ pause;
 % Set the file to be read in (change this to spamSample2.txt,
 % emailSample1.txt or emailSample2.txt to see different predictions on
 % different emails types). Try your own emails as well!
-filename = 'spamSample1.txt';
+filenames = {'emailSample1.txt', 'emailSample2.txt', 'spamSample1.txt', 'spamSample2.txt', 'myEmailSample.txt', 'mySpamSample.txt'};
+for filenameCell = filenames
+    filename = filenameCell{:};
 
-% Read and predict
-file_contents = readFile(filename);
-word_indices  = processEmail(file_contents);
-x             = emailFeatures(word_indices);
-p = svmPredict(model, x);
+    % Read and predict
+    file_contents = readFile(filename);
+    word_indices  = processEmail(file_contents);
+    x             = emailFeatures(word_indices);
+    p = svmPredict(model, x);
 
-fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
-fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
-
+    fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
+    fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
+end
